800506ed1a9f3e319dbf6182c4c2cbc6a7315100
Marc Remolt <m.remolt@webmasters.de>
2010-05-25 10:31:59 +0200

    Seminartermine eines Seminars können nun als Tabelle angezeigt werden

---
+++ seminartermine.php
@@ -0,0 +1,29 @@
+<?php
+    require_once 'lib/db_connect.inc.php';
+    require_once 'models/seminar.php';
+    require_once 'models/seminartermin.php';
+
+    Seminar::connect($db);
+    Seminartermin::connect($db);
+
+    session_start();
+
+    $title  = 'Tolle Seminarverwaltung';
+    $action = $_REQUEST['action'];
+
+    switch($action) {
+        case 'list':
+            $seminare = Seminar::getAll();
+            break;
+
+        case 'list_by_seminar':
+            $seminar = Seminar::getById($_REQUEST['seminar_id']);
+            $seminartermine = $seminar->getSeminartermine();
+            break;
+
+        default:
+            header('Location: seminartermine.php?action=list');
+            exit;
+    }
+
+    require_once 'views/seminartermine/' . $action . '.phtml';
